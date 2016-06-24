4c5110269601cec248739cd0bd5161f093fa7d3e
Leonardo Costa <leonhad@gmail.com>
2016-06-24 19:00:04 -0300

    Fixed Sonar path scanning.

--- sonar.sh
+++ sonar.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 rm -rf out/
-build-wrapper --out-dir out make clean all
-cppcheck -v --enable=all --xml -j 4 -I src src 2> out/cppcheck.xml
-rats -w 3 --xml src/ > out/rats.xml
-find src | grep ".*\.cpp\|.*\.h" | vera++ -d -s -c out/vera.xml
+build-wrapper --out-dir ../pdftools-sonar make clean all check -j
+cppcheck -v --enable=all --xml -j 4 -I src src test 2> ../pdftools-sonar/cppcheck.xml
+rats -w 3 --xml . > ../pdftools-sonar/rats.xml
+find . | grep ".*\.cpp\|.*\.h" | vera++ -d -s -c ../pdftools-sonar/vera.xml
 sonar-runner
 
