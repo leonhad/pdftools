e6e33a688510a9af439c4362607b4ba5434d984a
Leonardo Costa <leonhad@gmail.com>
2016-06-24 09:57:39 -0300

    Added vera++ parser.

--- sonar.sh
+++ sonar.sh
@@ -4,5 +4,6 @@ rm -rf out/
 build-wrapper --out-dir out make clean all
 cppcheck -v --enable=all --xml -j 4 -I src src 2> out/cppcheck.xml
 rats -w 3 --xml src/ > out/rats.xml
+find src | grep ".*\.cpp\|.*\.h" | vera++ -d -s -c out/vera.xml
 sonar-runner
 
