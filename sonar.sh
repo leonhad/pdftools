3f27c3ef13d98a0a80621096cd22301cff97bb5c
Leonardo Costa <leonhad@gmail.com>
2016-06-30 22:31:57 -0300

    Added code coverage test.

--- sonar.sh
+++ sonar.sh
@@ -5,6 +5,7 @@ build-wrapper --out-dir out/ make clean all
 ./test/testsuit
 xsltproc cppunit-1.x-to-junit-1.0.xsl cppunit.xml > out/cppunit.xml
 rm cppunit.xml
+gcovr -x --object-directory=. > out/gcov.xml
 cppcheck -v --enable=all --xml -I src src test 2> out/cppcheck.xml
 rats -w 3 --xml . > out/rats.xml
 find . | grep ".*\.cpp\|.*\.h" | vera++ -d -s -c out/vera.xml
