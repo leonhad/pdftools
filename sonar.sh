336960823585cff65bc661746eed01bd85e1e1fb
Leonardo Costa <leonhad@gmail.com>
2016-09-16 21:30:57 -0300

    Fixed some Sonar issues.

--- sonar.sh
+++ sonar.sh
@@ -2,12 +2,11 @@
 
 rm -rf out/
 build-wrapper --out-dir out/ make clean all
-./test/testsuit
-xsltproc cppunit-1.x-to-junit-1.0.xsl cppunit.xml > out/cppunit.xml
-rm cppunit.xml
-gcovr -x --object-directory=$(pwd) > out/gcov.xml
+make check
+xsltproc cppunit-1.x-to-junit-1.0.xsl src/cppunit.xml > out/cppunit.xml
+rm src/cppunit.xml
+gcovr -x -r src > out/gcov.xml
 cppcheck -v --enable=all --xml -I src src test 2> out/cppcheck.xml
 rats -w 3 --xml . > out/rats.xml
 find . | grep ".*\.cpp\|.*\.h" | vera++ -d -s -c out/vera.xml
-sonar-runner
-
+sonar-scanner
