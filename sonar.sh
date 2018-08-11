421d81436546aac03b70cd8889c85c9a8ddec4a5
Leonardo Costa <leonhad@gmail.com>
2018-08-11 20:04:34 -0300

    Fixed sonar tests

--- sonar.sh
+++ sonar.sh
@@ -3,7 +3,8 @@
 rm -rf out/
 build-wrapper --out-dir out/ make all
 make check -j
-tests/gtest --gtest_output=xml:out/xunit.xml
+valgrind --xml=yes --xml-file=out/valgrind.xml tests/gtest --gtest_output=xml:out/xunit.xml
+# tests/gtest --gtest_output=xml:out/xunit.xml
 gcovr -x -r . > out/gcov.xml
 cppcheck --quiet --check-config --enable=all --xml --xml-version=2 -I src src 2> out/cppcheck.xml
 rats -w 3 --xml src > out/rats.xml
