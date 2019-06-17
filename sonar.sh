265ad310df25f8de07901e158c0ccc07ddc5ba59
Leonardo Alves da Costa <leonhad@gmail.com>
2019-06-16 23:38:26 -0300

    Fixed cppcheck.

--- sonar.sh
+++ sonar.sh
@@ -5,8 +5,7 @@ build-wrapper --out-dir out/ make all
 make check -j
 valgrind --xml=yes --xml-file=out/valgrind.xml tests/gtest --gtest_output=xml:out/xunit.xml
 # tests/gtest --gtest_output=xml:out/xunit.xml
-gcovr -x -r . > out/gcov.xml
-cppcheck --quiet --check-config --enable=all --xml --xml-version=2 -I src src 2> out/cppcheck.xml
+cppcheck --quiet --suppress=missingIncludeSystem --check-config --enable=all --xml --xml-version=2 -I src src 2> out/cppcheck.xml
 rats -w 3 --xml src > out/rats.xml
 find . | grep ".*\.cpp\|.*\.h" | vera++ -d -s -c out/vera.xml
 sonar-scanner
