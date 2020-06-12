be3cc155fbcb3a5f0440055d2bf12c66a5c3490d
Leonardo Costa <leonhad@gmail.com>
2020-06-12 20:35:50 -0300

    Fixed the Visual Studio project.

--- src/getopt/getopt.h
+++ src/getopt/getopt.h
@@ -7,27 +7,31 @@
 #ifndef __GETOPT_H_
 #define __GETOPT_H_
 
-	#if defined(EXPORTS_GETOPT)
-		#pragma message("Exporting getopt library")
-		#define _GETOPT_API __declspec(dllexport)
-	#else
-		#pragma message("Importing getopt library")
-		#define _GETOPT_API __declspec(dllimport)
-	#endif
+#if defined(EXPORTS_GETOPT)
+#pragma message("Exporting getopt library")
+#define _GETOPT_API __declspec(dllexport)
+#else
+#pragma message("Importing getopt library")
+#define _GETOPT_API __declspec(dllimport)
+#endif
 
-	// Standard GNU options
-	#define	null_argument		0	/*Argument Null*/
-	#define	no_argument			0	/*Argument Switch Only*/
-	#define required_argument	1	/*Argument Required*/
-	#define optional_argument	2	/*Argument Optional*/	
+ // Standard GNU options
+#define	null_argument		0	/*Argument Null*/
+#define	no_argument			0	/*Argument Switch Only*/
+#define required_argument	1	/*Argument Required*/
+#define optional_argument	2	/*Argument Optional*/	
 
-	// Shorter Options
-	#define ARG_NULL	0	/*Argument Null*/
-	#define ARG_NONE	0	/*Argument Switch Only*/
-	#define ARG_REQ		1	/*Argument Required*/
-	#define ARG_OPT		2	/*Argument Optional*/
+// Shorter Options
+#define ARG_NULL	0	/*Argument Null*/
+#define ARG_NONE	0	/*Argument Switch Only*/
+#define ARG_REQ		1	/*Argument Required*/
+#define ARG_OPT		2	/*Argument Optional*/
 
-	#include <string.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 	extern _GETOPT_API int optind;
 	extern _GETOPT_API int opterr;
@@ -38,13 +42,17 @@
 	{
 		const char* name;
 		int has_arg;
-		int *flag;
+		int* flag;
 		int val;
 	};
 
-	extern _GETOPT_API char *optarg;
-	extern _GETOPT_API int getopt(int argc, char *const *argv, const char *optstring);
-	extern _GETOPT_API int getopt_long(int argc, char *const *argv, const char *options, const struct option *long_options, int *opt_index) ;
-	extern _GETOPT_API int getopt_long_only(int argc, char *const *argv, const char *options, const struct option *long_options, int *opt_index) ;
+	extern _GETOPT_API char* optarg;
+	extern _GETOPT_API int getopt(int argc, char* const* argv, const char* optstring);
+	extern _GETOPT_API int getopt_long(int argc, char* const* argv, const char* options, const struct option* long_options, int* opt_index);
+	extern _GETOPT_API int getopt_long_only(int argc, char* const* argv, const char* options, const struct option* long_options, int* opt_index);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
