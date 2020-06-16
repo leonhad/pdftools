cbf6fc58e70d5a10a909503b7715e68f463bce07
Leonardo Costa <leonhad@gmail.com>
2020-06-15 22:37:49 -0300

    Fixed build

---
+++ src/tests/main.cpp
@@ -0,0 +1,34 @@
+#include "littletest.h"
+
+#include "../parser/pageparser.h"
+#include "../nodes/nodes.h"
+#include <fstream>
+
+using namespace std;
+using namespace parser;
+using namespace node;
+
+LT_BEGIN_SUITE(ParserSuite)
+void set_up()
+{
+    //
+}
+
+void tear_down()
+{
+    //
+}
+LT_END_SUITE(ParserSuite)
+
+LT_BEGIN_TEST(ParserSuite, Stream1)
+
+ifstream filestream("stream1.txt", ios::binary);
+PageParser parser(&filestream);
+RootNode *root = parser.Parse();
+LT_ASSERT(root);
+filestream.close();
+LT_END_TEST(Stream1)
+
+LT_BEGIN_AUTO_TEST_ENV()
+    AUTORUN_TESTS()
+LT_END_AUTO_TEST_ENV()
